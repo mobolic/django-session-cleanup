ec9c50dbb4aa9acd1896df47288a1f5bddebdc13
Martey Dodoo <martey@mobolic.com>
2017-12-14 18:11:49 -0500

    Remove support for Django<1.5.

--- session_cleanup/tasks.py
+++ session_cleanup/tasks.py
@@ -4,14 +4,5 @@ from django.core import management
 
 @task
 def cleanup():
-    """
-    Cleanup expired sessions by using Django management command.
-
-    Since the command's name changed in Django 1.5, we must try using
-    both 'clearsessions' and 'cleanup'.
-
-    """
-    try:
-        management.call_command("clearsessions", verbosity=0)
-    except management.base.CommandError:
-        management.call_command("cleanup", verbosity=0)
+    """Cleanup expired sessions by using Django management command."""
+    management.call_command("clearsessions", verbosity=0)
