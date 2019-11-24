4561106dc017d5ee88c749fce051fb6756433a94
Martey Dodoo <martey@mobolic.com>
2019-11-24 18:42:04 -0500

    Bump version and update changelog.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.rst') as f:
 
 setup(
     name='django-session-cleanup',
-    version='1.0.0',
+    version='2.0.0',
     description=('A periodic task for removing expired Django sessions '
                  'with Celery.'),
     long_description=readme,
