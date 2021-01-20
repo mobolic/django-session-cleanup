91d5a20075c689e3cddd0d4c6bab1ef3149f1f2e
Martey Dodoo <martey@mobolic.com>
2021-01-20 14:10:37 -0500

    Upgrade tested Python and Django versions.
    Upgrade tested versions of Django and Python. Reorder entries so that
    they are by Django version (instead of Python version) to make upgrading
    in the future easier.

--- setup.py
+++ setup.py
@@ -18,16 +18,15 @@ setup(
     maintainer_email='martey+django-session-cleanup@mobolic.com',
     url='https://github.com/mobolic/django-session-cleanup',
     classifiers=[
-        'Framework :: Django :: 1.11',
-        'Framework :: Django :: 2.0',
-        'Framework :: Django :: 2.1',
         'Framework :: Django :: 2.2',
         'Framework :: Django :: 3.0',
+        'Framework :: Django :: 3.1',
         'License :: OSI Approved :: BSD License',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
         'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
     ],
     packages=find_packages(exclude=('tests',))
 )
