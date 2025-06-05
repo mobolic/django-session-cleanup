49b1bf988c451984e79bea4988ae425ff4c66ed9
Martey Dodoo <martey@mobolic.com>
2025-06-05 18:45:26 -0400

    Update tested versions of Python and Django

--- setup.py
+++ setup.py
@@ -18,15 +18,15 @@ setup(
     maintainer_email='martey+django-session-cleanup@mobolic.com',
     url='https://github.com/mobolic/django-session-cleanup',
     classifiers=[
-        'Framework :: Django :: 2.2',
-        'Framework :: Django :: 3.0',
-        'Framework :: Django :: 3.1',
+        'Framework :: Django :: 4.2',
+        'Framework :: Django :: 5.1',
+        'Framework :: Django :: 5.2',
         'License :: OSI Approved :: BSD License',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     packages=find_packages(exclude=('tests',))
 )
